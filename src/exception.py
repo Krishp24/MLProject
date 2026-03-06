734ff108dc253a4f79e4be1b400b8105aa4c66a2
Krishp24 <Krishp0435@gmail.com>
2026-03-06 10:50:33 +0530

    logging and custom exception

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import sys
+import logging
+from logger import *
+
+
+def error_message_detail(error, error_detail: sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_messsge = str(error)
+    error_message = "Error occurred in python script name [{}] line number [{}] error message [{}]".format(file_name, line_number, error_messsge)
+    
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+        
+    def __str__(self):
+        return self.error_message
+    
